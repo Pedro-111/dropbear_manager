fefe8ad4a5856463d0d4cb64c06aa3245ed12a5c
Pedro-111 <108990464+Pedro-111@users.noreply.github.com>
2024-08-31 21:23:18 -0500

    Update dropbear_manager.sh

--- dropbear_manager.sh
+++ dropbear_manager.sh
@@ -124,7 +124,7 @@ restart_dropbear() {
     sleep 2
     if ! $(need_sudo) service dropbear start; then
         echo -e "${RED}Error al reiniciar Dropbear. Mostrando logs:${NC}"
-        $(need_sudo) service dropber status
+        $(need_sudo) service dropbear status
         $(need_sudo) journalctl -xeu dropbear.service
     else
         echo -e "${GREEN}Dropbear reiniciado con éxito${NC}"
